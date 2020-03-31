3137bab8d9d94c9e4a2b4c8ac8b11fa28ab8b410
Maxwell <eumaxwell@gmail.com>
2020-03-31 14:52:23 -0300

    frontend da aplicação

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+//import Header from "./Header";
+import Routes from "./routes";
+import "./global.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <Routes/>;
 }
+
+/**
+ *   let [counter, SetCounter] = useState(0);
+
+  function increment() {
+    SetCounter(counter+1);
+  }
+
+  return (
+      <div>
+      <Header title="Omministack 11">Children {counter}</Header>
+      <button onClick={increment}>Incremento</button>
+    </div>
+ */
 
 export default App;
